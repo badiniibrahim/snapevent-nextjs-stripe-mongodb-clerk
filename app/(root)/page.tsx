12b8b39595297304c66e3ae5286ac9a101a8d298
Ibrahimovic <ibrahimovic@MacBook-Air-de-Ibrahimovic.local>
2023-12-15 18:08:33 +0100

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+export default function Home() {
+  return (
+    <div>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">
+              Organize and celebrate your events with our platform!{" "}
+            </h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Book and learn helpful tips from 3,168+ mentors in world-class
+              companies with our global community.
+            </p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">Explore Now</Link>
+            </Button>
+          </div>
+
+          <Image
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
